ddf82ee36c9ffab83c25a456350390c39e65da8f
Oprean Sergiu <oprean@gmail.com>
2015-07-18 08:02:21 +0300

    fix slim routes

--- api/index.php
+++ api/index.php
@@ -1,14 +1,11 @@
 <?php
 require_once '../php/bootstrap.php';
 
-R::setup( 'sqlite:'.ROOT_DIR.'/assets/data/quizzes.sqlite' );
+R::setup( 'sqlite:'.ROOT_DIR.'/assets/data/raspi.sqlite' );
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 $app->config('debug', DEBUG_MODE);
 
-require_once(ROOT_DIR.'/php/routes/results.php');
-require_once(ROOT_DIR.'/php/routes/mail.php');
-require_once(ROOT_DIR.'/php/routes/pdf.php');
 require_once(ROOT_DIR.'/php/routes/info.php');
 
 $app->run();
