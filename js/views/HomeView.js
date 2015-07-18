8154fbe5b94a795172f2e00917d6dd9ff93e88ce
Oprean Sergiu <oprean@gmail.com>
2015-07-18 15:28:32 +0300

    root_temp ... to room_temp :)

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -11,17 +11,17 @@ define([
 		className : 'bg-raspi-logo',
 		initialize : function(options) {
 			var self = this;
-			
+			this.cpu_temp = 0;
+			this.room_temp = 0;
 			var tempCmd = new Command({id:'temp'});
 			tempCmd.fetch({
 				success: function(model) {
 					if (model.get('status') == 'success') {
-						self.cpu_temp = model.get('response');				
-						self.root_temp = parseFloat(self.cpu_temp) - 21.6;
+						self.cpu_temp = parseFloat(model.get('response'));				
+						self.room_temp = self.cpu_temp - 21.6;
 					} else {
-						self.cpu_temp = null;
-						self.room_temp = null;
-						
+						self.cpu_temp = 0;
+						self.room_temp = 0;
 					}
 					self.render();
 				}
@@ -30,8 +30,8 @@ define([
 		
 		templateHelpers: function() {
 			return {
-				cpu_temp: this.cpu_temp,
-				room_temp: this.room_temp,
+				cpu_temp: this.cpu_temp.toFixed(2),
+				room_temp: this.room_temp.toFixed(2),
 			};
 		}
 	});
