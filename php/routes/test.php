3c4ee57dd37fb4b795811d00450b16f96e247dab
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-07-20 11:29:16 +0300

    testing gpiomode

--- php/routes/test.php
+++ php/routes/test.php
@@ -1,5 +1,5 @@
 <?php
-$app->get('/test', function () use ($app) {
+$app->get('/request', function () use ($app) {
 	//echo RUNTIME_DIR;
 	$req = $app->request;
 
@@ -14,4 +14,10 @@ $app->get('/test', function () use ($app) {
 	echo str_replace('/api', '/', $req->getUrl().$req->getRootUri());
 	
 });
+
+$app->get('/gpiomode/:pin', function ($pin) use ($app) {
+	echo file_get_contents('/sys/class/gpio/gpio'.($pin + 0).'/direction');	
+});
+
+
 ?>
