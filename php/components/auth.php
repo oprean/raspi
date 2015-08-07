ca57225a59d4a133b540ef789e42998037310771
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-08-07 09:32:48 +0300

    auth middleware

---
+++ php/components/auth.php
@@ -0,0 +1,47 @@
+<?php
+
+class TokenAuth extends \Slim\Middleware {
+
+    public function __construct() {}
+
+    /**
+     * Deny Access
+     */
+    public function deny_access() {
+        $res = $this->app->response();
+        $res->status(401);
+    }
+
+    /**
+     * Check against the DB if the token is valid
+     * 
+     * @param string $token
+     * @return bool
+     */
+    public function authenticate($token) {
+        return User::validateToken($token);
+    }
+
+    /**
+     * Call
+     */
+    public function call() {
+        //Get the token sent from jquery
+        $tokenAuth = $app->request->headers->get('Authorization');
+        
+        //Check if our token is valid
+        if ($this->authenticate($tokenAuth)) {
+            //Get the user and make it available for the controller
+            $usrObj = new User();
+            $usrObj->getByToken($tokenAuth);
+            $this->app->auth_user = $usrObj;
+            //Update token's expiration
+            User::keepTokenAlive($tokenAuth);
+            //Continue with execution
+            $this->next->call();
+        } else {
+            $this->deny_access();
+        }
+    }
+
+}
