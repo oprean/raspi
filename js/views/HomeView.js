d8916eea682450b8861811967633fcc59d9da508
Oprean Sergiu <oprean@gmail.com>
2015-08-13 00:01:31 +0300

    home redesign with power switch

--- js/views/HomeView.js
+++ js/views/HomeView.js
@@ -5,34 +5,42 @@ define([
   'backbone.marionette',
   'text!templates/home.html',
   'models/Command',
-], function($, _, Backbone, Marionette, homeTpl, Command){
+  'models/Pin',
+], function($, _, Backbone, Marionette, homeTpl, Command, Pin){
 	var HomeView = Backbone.Marionette.ItemView.extend({
 		template : _.template(homeTpl),
 		className : 'bg-raspi-logo',
+		
+		events: {
+			'click #power1' : 'powerSwitch'
+		},
+		
 		initialize : function(options) {
 			var self = this;
 			this.cpu_temp = 0;
 			this.room_temp = 0;
+			this.pin = new Pin({id: 23});
+			this.pin.fetch({async:false}); 
 			var tempCmd = new Command({id:'tempsensor'});
 			tempCmd.fetch({
+				async:false,
 				success: function(model) {
 					if (model.get('status') == 'success') {	
 						self.room_temp = parseFloat(model.get('response'));
 					} else {
 						self.room_temp = 0;
 					}
-					self.render();
 				}
 			});
 			tempCmd = new Command({id:'temp'});
 			tempCmd.fetch({
+				async:false,
 				success: function(model) {
 					if (model.get('status') == 'success') {	
 						self.cpu_temp = parseFloat(model.get('response'));
 					} else {
 						self.cpu_temp = 0;
 					}
-					self.render();
 				}
 			});
 		},
@@ -61,8 +69,15 @@ define([
 					}
 
 				});
-				var chartView = new ZingChart.ZingChartView({model: chartData, el: this.$('#room-temp-gauge-container')});
+				var chartView = new ZingChart.ZingChartView({model: chartData, el: this.$('#temp-gauge-container')});
 				chartView.render();		
+		},
+		
+		powerSwitch: function(e) {
+			console.log('toggle val: ' + this.pin.get('Phys'));
+			var value = (this.pin.get('Value') == 0)?1:0;
+			this.pin.save({Value:value}, {patch:true});
+			this.render();
 		},
 		
 		templateHelpers: function() {
