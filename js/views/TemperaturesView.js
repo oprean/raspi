1fdbea642adb492dc67b4b43f01c17b6e12062a5
Oprean Sergiu <oprean@gmail.com>
2015-08-13 22:53:35 +0300

    limit to last N days

--- js/views/TemperaturesView.js
+++ js/views/TemperaturesView.js
@@ -10,7 +10,7 @@ define([
 		template : _.template(statsTpl),
 		initialize : function(options) {
 			var self = this;
-			$.getJSON('api/temperatures/144', function(data){
+			$.getJSON('api/temperatures/3', function(data){
 				self.temperatures = self.prepareData(data);
 				self.render(); 	
 			});
--- php/routes/temperature.php
+++ php/routes/temperature.php
@@ -1,11 +1,12 @@
 <?php
 define('TEMPERATURE_BEAN', 'temperature');
 
-$app->get('/temperatures(/:limit)', function ($limit) use ($app) {
-	$limit = empty($limit)?9999:$limit;
-    $items = R::find(TEMPERATURE_BEAN, ' ORDER BY date desc LIMIT ?', array($limit));
+$app->get('/temperatures(/:days)', function ($days=null) use ($app) {
+	$days = empty($days)?356:$days;
+	$sql = "SELECT * FROM temperature where date >= date('now', '-$days day') ORDER BY date desc";
+    $items = R::getAll($sql);
     $app->response()->header('Content-Type', 'application/json');
-    echo json_encode(R::exportAll($items));
+    echo json_encode($items);
 });
 
 $app->get('/temperature/:id', function ($id) use ($app) {
