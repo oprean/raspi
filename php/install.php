9c33104e9f94c3b6cb0669a43249d1f8f184a98a
Oprean Sergiu <oprean@gmail.com>
2015-09-04 18:00:15 +0300

    install work

--- php/install.php
+++ php/install.php
@@ -1,12 +1,12 @@
 <?php
 require_once('bootstrap.php');
-R::setup( 'sqlite:'.ROOT_DIR.'../data/raspi.sqlite' );
+R::setup( 'sqlite:'.ROOT_DIR.'/data/raspi.sqlite' );
 
 // create admin user
 $item = R::dispense(USER_BEAN); 
 $item->username = 'admin';
-//$item->password = password_hash('admin',  PASSWORD_DEFAULT);
-$item->password = 'admin';
+$item->password = password_hash('admin',  PASSWORD_DEFAULT);
+//$item->password = 'admin';
 $item->name = 'Sergiu';
 $item->email = 'oprean@gmail.com';
 $item->token = 'admin';
