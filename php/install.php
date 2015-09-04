1c6b80b78ba765ce7d75004208ca4041653411f4
Oprean Sergiu <oprean@gmail.com>
2015-09-04 18:05:42 +0300

    pass hash

--- php/install.php
+++ php/install.php
@@ -5,8 +5,7 @@ R::setup( 'sqlite:'.ROOT_DIR.'/data/raspi.sqlite' );
 // create admin user
 $item = R::dispense(USER_BEAN); 
 $item->username = 'admin';
-$item->password = password_hash('admin',  PASSWORD_DEFAULT);
-//$item->password = 'admin';
+$item->password = hash('admin',  PASSWORD_DEFAULT);
 $item->name = 'Sergiu';
 $item->email = 'oprean@gmail.com';
 $item->token = 'admin';
