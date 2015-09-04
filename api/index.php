12b7c07ed204eed9e5558c09c25b39310354fe46
unknown <sergiu.oprean@SERGIU-PC.bitalb.ro>
2015-09-04 12:45:27 +0300

    disable auth for now

--- api/index.php
+++ api/index.php
@@ -5,7 +5,7 @@ R::setup( 'sqlite:'.ROOT_DIR.'/data/raspi.sqlite' );
 \Slim\Slim::registerAutoloader();
 $app = new \Slim\Slim();
 $app->config('debug', DEBUG_MODE);
-$app->add(new TokenAuth());
+//$app->add(new TokenAuth());
 
 require_once(ROOT_DIR.'/php/routes/command.php');
 require_once(ROOT_DIR.'/php/routes/gpio.php');
