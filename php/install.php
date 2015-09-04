d8d7a639ff88a4981c12ab0273ea77056453578d
Oprean Sergiu <oprean@gmail.com>
2015-09-04 18:09:13 +0300

    still on install

--- php/install.php
+++ php/install.php
@@ -5,11 +5,11 @@ R::setup( 'sqlite:'.ROOT_DIR.'/data/raspi.sqlite' );
 // create admin user
 $item = R::dispense(USER_BEAN); 
 $item->username = 'admin';
-$item->password = hash('admin',  PASSWORD_DEFAULT);
+$item->password = hash('sha256', 'admin');
 $item->name = 'Sergiu';
 $item->email = 'oprean@gmail.com';
-$item->token = 'admin';
-$item->token_expire = date('Y-m-d H:i:s', strtotime('+1 hour'));
+$item->token = null;
+$item->token_expire = date('Y-m-d H:i:s');
 R::store($item);
 R::close();
 ?>
